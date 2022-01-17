9bfef1e3d2da5adc72e13cc9f2802c1f38574a8c
Meri <mgogi13@freeuni.edu.ge>
2022-01-17 15:53:13 +0400

    Fix Linter Issues

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -3,7 +3,9 @@ import React from 'react';
 const BookInput = () => (
   <form>
     <input type="text" placeholder="Add a Book..." value="" name="title" />
-    <button className="input-remove">Remove</button>
+    <button type="button" className="input-remove">
+      Remove
+    </button>
   </form>
 );
 
