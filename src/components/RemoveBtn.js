328b3c5c4d3991a637c033c896792f40cda83a39
Meri <mgogi13@freeuni.edu.ge>
2022-01-20 19:06:55 +0400

    Add category

--- src/components/RemoveBtn.js
+++ src/components/RemoveBtn.js
@@ -4,12 +4,6 @@ import { deleteBookFromAPI } from '../redux/books/books';
 const RemoveBtn = (props) => {
   const dispatch = useDispatch();
 
-  // const bookToRemove = (e) => {
-  //   const bookId = {
-  //     id: e.target.id,
-  //   };
-  //   dispatch(deleteBookFromAPI(bookId));
-  // };
   const id = props;
   return (
     <button type="button" onClick={() => dispatch(deleteBookFromAPI(id.id))} id={id.id}>
