e82cc0847f50dea6b621b19adc3e792944856884
Meri <mgogi13@freeuni.edu.ge>
2022-01-20 19:00:33 +0400

    Add interactions with API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookItem from './BookItem';
+import { getBookFromAPI } from '../redux/books/books';
 
 const BookList = () => {
   const data = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBookFromAPI());
+  }, []);
   return (
     <ul>
       {data.map((book) => (
-        <BookItem key={book.id} data={book} />
+        <BookItem key={book.id} data={book} category={book.category} />
       ))}
     </ul>
   );
--- src/components/RemoveBtn.js
+++ src/components/RemoveBtn.js
@@ -1,18 +1,18 @@
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { deleteBookFromAPI } from '../redux/books/books';
 
 const RemoveBtn = (props) => {
   const dispatch = useDispatch();
 
-  const bookToRemove = (e) => {
-    const bookId = {
-      id: e.target.id,
-    };
-    dispatch(removeBook(bookId));
-  };
+  // const bookToRemove = (e) => {
+  //   const bookId = {
+  //     id: e.target.id,
+  //   };
+  //   dispatch(deleteBookFromAPI(bookId));
+  // };
   const id = props;
   return (
-    <button type="button" onClick={bookToRemove} id={id.id}>
+    <button type="button" onClick={() => dispatch(deleteBookFromAPI(id.id))} id={id.id}>
       Remove Book
     </button>
   );
