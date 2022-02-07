c69f1e170c47456b97625ae07953d8e2199a576f
Meri <mgogi13@freeuni.edu.ge>
2022-02-07 19:00:00 +0400

    Create New category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { sendCategoryToAPI } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <h2>Under construction</h2>
-  </div>
-);
+const Categories = () => {
+  const data = useSelector((state) => state.categoriesReducer);
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
+  console.log(data, 'data from categories');
+  const addCategory = (e) => {
+    e.preventDefault();
+    const newCategory = {
+      name: category,
+    };
+    dispatch(sendCategoryToAPI(newCategory));
+    setCategory('');
+  };
+
+  return (
+    <div>
+      <p>If you don&#39;t see in the list a category of your choice, add one</p>
+      <input
+        type="text"
+        placeholder="Add a Category..."
+        value={category}
+        name="title"
+        onChange={(e) => setCategory(e.target.value)}
+        required
+      />
+      <button type="button" className="input-add" onClick={addCategory}>
+        Add Category
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const GET_CATEGORY = 'bookStore/books/GET_CATEGORY';
+const ADD_CATEGORY = 'bookStore/books/ADD_CATEGORY';
 const initialState = [];
 
 const baseURL2 = 'http://18.197.248.204:2020/api/category';
@@ -10,13 +11,34 @@ export const getCategory = (payload) => ({
   payload,
 });
 
+export const addCategory = (payload) => ({
+  type: ADD_CATEGORY,
+  payload,
+});
+
 export const getCategoryFromAPI = () => async (dispatch) => {
   const category = await axios.get(`${baseURL2}/getAll`);
   dispatch(getCategory(category));
 };
 
+export const sendCategoryToAPI = (payload) => async (dispatch) => {
+  console.log(payload, 'this is payload');
+  const { name } = payload;
+
+  const newCategory = {
+    name,
+  };
+  await axios.post(
+    `${baseURL2}/create`,
+    newCategory,
+  );
+  dispatch(addCategory(payload));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_CATEGORY:
+      return [...state, action.payload];
     case GET_CATEGORY:
       return [...action.payload.data];
     default:
